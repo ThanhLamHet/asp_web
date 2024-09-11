3f7904ec2ee2178cd147fc2cbd9e051e310ae5c7
ThanhLamHet <Thanhpro347@gmail.com>
2024-09-11 22:42:01 +0700

    load BTVN2

---
+++ BTVN02/Controllers/.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTVN02.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Trần Trọng Thanh ";
+            ViewBag.MSSV = " 1822041609";
+            ViewBag.Nam = "Năm 2024";
+            return View();
+        }
+    }
+}
