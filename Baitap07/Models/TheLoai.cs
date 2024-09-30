bf6f9139e8e1d29492bfea3e0262b2f754497fb5
ThanhLamHet <Thanhpro347@gmail.com>
2024-09-30 14:13:09 +0700

    load btap07

---
+++ Baitap07/Models/TheLoai.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Baitap07.Models
+{
+    public class TheLoai
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Khong duoc de trong ten The Loai")]
+        [Display(Name ="The Loai")]
+
+
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Khong dung dinh dang ngay")]
+
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+    }
+}
